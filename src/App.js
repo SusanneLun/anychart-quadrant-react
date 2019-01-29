f54b899c1c66123999bde25fc912a577efebff41
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-01-29 18:15:34 +0000

    Rectangular but working

--- src/App.js
+++ src/App.js
@@ -4,37 +4,39 @@ import './App.css';
 import AnyChart from 'anychart-react'
 import anychart from 'anychart'
 
-const data = [{x: 7, y: 4, name: "Tom"}, {x: 20, y: 7, name: "Tim"}, {x: 90, y: 90, name: "Tam"}]
+const data = [{x: 7, value: 4, name: "Tom"}, {x: 2, value: 7, name: "Tim"}, {x: 9, value: 9, name: "Tam"}, {x: 3, value: 5, name: "Eric"}]
 const quarters = {
   rightTop: {
     title: {
-      text: 'LEADERS',
+      text: '',
       fontColor: '#ff8f00',
       padding: 10
     }
   },
   rightBottom: {
     title: {
-      text: 'VISIONARIES',
+      text: '',
       fontColor: '#ff8f00',
       padding: 10
     }
   },
   leftTop: {
     title: {
-      text: 'CHALLENGERS',
+      text: '',
       fontColor: '#ff8f00',
       padding: 10
     }
   },
   leftBottom: {
     title: {
-      text: 'NICHE PLAYERS',
+      text: '',
       fontColor: '#ff8f00',
       padding: 10
     }
   }
 }
+
+
 
 
 class App extends Component {
@@ -44,8 +46,33 @@ class App extends Component {
     }
 
   render() {
-    var chart = anychart.quadrant();
-    const dataSet = anychart.data.set(this.state.chartData);  
+
+    let chart = anychart.quadrant();
+    let yTitle = chart.yAxis().title();
+    yTitle.enabled(true);
+    yTitle.text("Power Rating --->");
+    yTitle.align("bottom");
+
+    let yScale = anychart.scales.linear();
+
+
+    let yAxis = chart.yAxis(0);
+    yAxis.scale(yScale);
+
+
+    chart.xAxis().title("Interest Rating --->");
+
+    chart.yScale().minimum(0);
+    chart.yScale().maximum(10);
+    chart.xScale().minimum(0);
+    chart.xScale().maximum(10)
+
+    chart.title("Power/Interest Ratings")
+    chart.container("container")
+    chart.draw();
+
+
+    const dataSet = anychart.data.set(this.state.chartData);
     console.log(dataSet)
     var markers = chart.marker(dataSet);
     // set labels settings
@@ -55,8 +82,8 @@ class App extends Component {
       .fontColor('#546e7a')
       .position('right')
       .anchor('left-center')
-      .offsetX(0)
-      .offsetY(0)
+      .offsetX(2)
+      .offsetY(2)
       .format('{%Name}');
     // disabled tooltip
     markers.tooltip(false);
@@ -66,18 +93,9 @@ class App extends Component {
 
     return (
       <div className="App">
-       
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <AnyChart
             width='100%'
-            height='100%' 
+            height='100%'
             id='bar-chart'
             instance={chart}
         />
